e44172f52788afa629a95a1a3b9c5255a6cdac73
Patrick Stahl <p.stahl@shopware.com>
2019-11-27 08:34:41 +0100

    SW-24724 - Add config factory compatibility

--- Services/ModifiedConfigFactory.php
+++ Services/ModifiedConfigFactory.php
@@ -2,24 +2,28 @@
 
 namespace SwagCookieConsentManager\Services;
 
-use Shopware\Components\DependencyInjection\Bridge\Config;
-use Shopware\Components\ShopwareReleaseStruct;
-
-class ModifiedConfigFactory extends Config
+class ModifiedConfigFactory
 {
-    public function factory(\Zend_Cache_Core $cache, \Enlight_Components_Db_Adapter_Pdo_Mysql $db = null, $config = [], ShopwareReleaseStruct $release)
+    public function factory()
     {
+        $arguments = func_get_args();
+
+        list($cache, $db, $config) = $arguments;
+
         if (!$db) {
             return null;
+        }
+
+        if (isset($arguments[3]) && $arguments[3] instanceof \Shopware\Components\ShopwareReleaseStruct) {
+            $config['release'] = $arguments[3];
         }
 
         if (!isset($config['cache'])) {
             $config['cache'] = $cache;
         }
+
         $config['db'] = $db;
-        $config['release'] = $release;
 
         return new ConfigModified($config);
     }
-
 }
